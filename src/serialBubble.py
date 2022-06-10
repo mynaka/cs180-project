c9ee8087f564992a3f0aa6924bc152b25d2792b1
mynaka <avillanueva@up.edu.ph>
2022-06-10 17:36:16 +0800

    add documentation and implement timer

--- src/serialBubble.py
+++ src/serialBubble.py
@@ -1,20 +1,31 @@
 import random
+import time
 
 def serialbubbleSort(array):
+    '''
+    #Sort the list of integers through the Bubble Sort Algorithm (Serial Implementation)
+    '''
     size = len(array)
 
     for i in range(size):
-        swap = False
+        swap = False            #reset swap flag to false
         for j in range(size-i-1):
             if array[j] > array[j+1]:
-                array[j], array[j+1] = array[j+1], array[j]
-                swap = True
-        if not swap:
-            break
+                array[j], array[j+1] = array[j+1], array[j] #Swap list elements
+                swap = True                                 #Set swap flag to true
+        if not swap:       #If swap remains false, break loop
+            break          #This means that the array is already sorted
     return array
 
 N = int(input("Enter array size: "))
-unsorted = []
+unsortedArray = []
 for i in range(N):
-    unsorted.append(random.randint(-N//2, N//2))
-print(serialbubbleSort(unsorted))
+    unsortedArray.append(random.randint(-N//2, N//2))   #Generate rnaodmized array
+
+#Set timer and run function
+start = time.time()
+sortedArray = serialbubbleSort(unsortedArray)
+end = time.time()
+
+#print(sortedArray)      #Print sorted array
+print("Time elapsed:", end-start)
