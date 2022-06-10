d60744967f20c1641f5fde4fdcdecf8955c17ec9
mynaka <avillanueva@up.edu.ph>
2022-06-10 20:47:07 +0800

    add serial merge sort algorithm

---
+++ src/serialMerge.py
@@ -0,0 +1,58 @@
+from copy import deepcopy
+import random
+import time
+
+def serialmergeSort(array):
+    '''
+    Sort the list of integers through the Merge Sort Algorithm (Serial Implementation)
+    '''
+    i = 0           #to keep track of the index to be changed
+    if len(array) > 1:
+        # Finding the mid of the array
+        mid = len(array)//2
+
+        L = array[:mid]
+        R = array[mid:]
+ 
+        # recursively sorting the both halves 
+        serialmergeSort(L)
+        serialmergeSort(R)
+
+ 
+        # merge sorted arrays (while sorting)
+        while (len(L) > 0) and (len(R) > 0):
+            if(L[0] >= R[0]):
+                array[i] = R[0]
+                R.pop(0)
+            elif(R[0] >= L[0]):
+                array[i] = L[0]
+                L.pop(0)
+            i+=1
+
+        #incorporate whatever that's left
+        while(len(L) > 0):
+            array[i] = (L[0])
+            L.pop(0)
+            i+=1
+        
+        while(len(R) > 0):
+            array[i] = (R[0])
+            R.pop(0)
+            i+=1
+ 
+
+ 
+N = int(input("Enter array size: "))
+array = []
+for i in range(N):
+    array.append(random.randint(-N//2, N//2))   #Generate rnaodmized array
+unsortedArray = deepcopy(array)
+
+#Set timer and run function
+start = time.time()
+serialmergeSort(array)
+end = time.time()
+
+#print("Unsorted Arrray:",unsortedArray)
+#print("Sorted Array:   ",array)      #Print sorted array
+print("Time elapsed:", end-start)
